4e249730e2507eebfdc0872ec4fb40a9330835ec
Rizqirazkafi <rizqirazkafi56@gmail.com>
2021-06-12 18:37:57 +0700

    Added package install script

---
+++ install_script/install_package_qtile_radeon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Things to install in arcolinuxB QTile edition
+sudo pacman -S vivaldi --noconfirm --needed
+sudo pacman -S vivaldi-ffmpeg-codecs --noconfirm --needed
+sudo pacman -S code --noconfirm --needed
+sudo pacman -S acpilight --noconfirm --needed
+sudo pacman -S alacritty --noconfirm --needed
+sudo pacman -S pcmanfm --noconfirm --needed
+sudo pacman -S vlc --noconfirm --needed
+sudo pacman -S deadbeef --noconfirm --needed
+sudo pacman -S virtualbox --noconfirm --needed
