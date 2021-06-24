0c5e1ecbc822977af28f28573c5399aa1b984a17
Rizqirazkafi <rizqirazkafi56@gmail.com>
2021-06-24 19:37:14 +0700

    Update arcolinux install script

--- install_script/install_package_qtile_radeon.sh
+++ install_script/install_package_qtile_radeon.sh
@@ -9,11 +9,18 @@ sudo pacman -S alacritty --noconfirm --needed
 sudo pacman -S pcmanfm --noconfirm --needed
 sudo pacman -S vlc --noconfirm --needed
 sudo pacman -S deadbeef --noconfirm --needed
-sudo pacman -S virtualbox --noconfirm --needed
+sudo pacman -S virtualbox virtualbox-guest-iso --noconfirm --needed
+sudo gpasswd -a $USER vboxusers
+sudo systemctl enable vboxweb.service  
+yay -S virtualbox-ext-oracle
 sudo pacman -S fira-code --noconfirm --needed
+cp .bashrc ~/
+cp .vimrc ~/
+cp alacritty.yml ~/.config/alacritty/
 
+# Install Fetch Master 6000
 
-
+bash -c "$(curl https://raw.githubusercontent.com/anhsirk0/fetch-master-6000/master/install.sh)"
 
 
 
